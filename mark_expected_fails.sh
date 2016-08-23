dc1732ab1bce3007e6b4dcac59d12e598be6bafc
Shing Lyu <shing.lyu@gmail.com>
2016-08-23 14:43:35 +0800

    Script for marking expected fails
    MozReview-Commit-ID: BFlL5aUfPip

---
+++ mark_expected_fails.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mark_as_fail() {
+  grep -R "${1}" . --include *-stylo.list -l | xargs -L1 sed -i "/$1/s/^/fails /"
+}
+
+# Too many arguments breaks xargs, so we use for loop
+for i in $(grep 'UNEXPECTED-FAIL' "${1}" | grep -Po '(?<=\| )(.*)(?===)' | xargs -L1 -I{} basename {})
+do
+  echo "${i}"
+  mark_as_fail "${i}"
+done
+
+while grep -R "fails fails" --include *-stylo.list -q
+do
+  echo "Clean up multiple fails"
+  find . -name *-stylo.list | xargs sed -i 's/fails fails/fails/g'
+done
+
+find . -name *-stylo.list | xargs sed -i 's/^fails #/#/g'
+find . -name *-stylo.list | xargs sed -i 's/^fails load/skip load/g'
+# Manually change "fails (.*) load" to skip
