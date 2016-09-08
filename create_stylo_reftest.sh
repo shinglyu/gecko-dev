170868d43b08180191ceb085b0f8508874a61693
Shing Lyu <shing.lyu@gmail.com>
2016-09-08 14:02:44 +0800

    Updated script
    MozReview-Commit-ID: 9uTjvpRJ3vo

--- create_stylo_reftest.sh
+++ create_stylo_reftest.sh
@@ -1,114 +1,129 @@
-#!/usr/bin/env bash
-
-create_stylo_reftestlist(){
-  # Cleanup
-  find . -name ${1}-stylo.list | xargs rm
-
-  # Copy reftest.list to reftest-stylo.list
-  find . -name ${1}.list | xargs -I '{}' cp {} {}.stylo-gecko
-  find . -name ${1}.list.stylo-gecko | xargs rename "s/${1}.list.stylo-gecko/${1}-stylo.list/"
-
-  # Add a comment
-  find . -name ${1}-stylo.list | xargs -I{} bash -c "echo '# DO NOT EDIT! This is a auto-generated temporary list for Stylo testing' | cat - {} > /tmp/out && mv /tmp/out {}"
-
-  # Make all tests to be expected equal
-  find . -name ${1}-stylo.list | xargs sed -i "s/!= /== /g"
-  # Wrap inline comment to a new line for easier awk processing
-  find . -name ${1}-stylo.list | xargs perl -i -pe 's/^(.*?) (#.*)/$2\n$1/'
-  # "== A.html A-ref.html" => "== A.html A.html"
-  find . -name ${1}-stylo.list | xargs gawk -i inplace '{if (/==/) {$(NF)=$(NF-1); print} else {print}}'
-  ## Change all "include reftest.list" lines to "include reftest-stylo.list"
-  find . -name ${1}-stylo.list | xargs sed -i "s/${1}.list/${1}-stylo.list/g"
-
-  # TODO: ref-pref(x) test-pref(y) ... => ref-pref(y) test-pref(y) ...
-  # Use this to check if any file becomes empty due to errors
-  # find -name ${1}-stylo.list | xargs wc -l |sort -n -r
-}
-
-# There are reftest lists that is not named reftest.list
-# Use this script to find them:
-# find . -name reftest.list | xargs grep "^include" -h | grep -v "reftest.list" | sort | uniq
-
-REFTEST_LISTS="
-reftest
-default-preferences-tests
-reftest_border_abspos
-reftest_border_parent
-reftest_margin_abspos
-reftest_margin_parent
-reftest_padding_abspos
-reftest_padding_parent
-reftest_plain
-scripttests
-urlprefixtests
-"
-
-for i in ${REFTEST_LISTS}; do
-  echo "Processing ${i}"
-  create_stylo_reftestlist ${i}
-done
-
-for i in ${REFTEST_LISTS}; do
-  for j in ${REFTEST_LISTS}; do
-    echo "Replacing  string ${j} in all ${i}.list"
-    find . -name ${i}-stylo.list | xargs sed -i "s/${j}.list/${j}-stylo.list/g"
-  done
-done
+# #!/usr/bin/env bash
+#
+# create_stylo_reftestlist(){
+#   # Cleanup
+#   find . -name ${1}-stylo.list | xargs rm
+#
+#   # Copy reftest.list to reftest-stylo.list
+#   find . -name ${1}.list | xargs -I '{}' cp {} {}.stylo-gecko
+#   find . -name ${1}.list.stylo-gecko | xargs rename "s/${1}.list.stylo-gecko/${1}-stylo.list/"
+#
+#   # Add a comment
+#   find . -name ${1}-stylo.list | xargs -I{} bash -c "echo '# DO NOT EDIT! This is a auto-generated temporary list for Stylo testing' | cat - {} > /tmp/out && mv /tmp/out {}"
+#
+#   # Make all tests to be expected equal
+#   find . -name ${1}-stylo.list | xargs sed -i "s/!= /== /g"
+#   # Wrap inline comment to a new line for easier awk processing
+#   find . -name ${1}-stylo.list | xargs perl -i -pe 's/^(.*?) (#.*)/$2\n$1/'
+#   # "== A.html A-ref.html" => "== A.html A.html"
+#   find . -name ${1}-stylo.list | xargs gawk -i inplace '{if (/==/) {$(NF)=$(NF-1); print} else {print}}'
+#   ## Change all "include reftest.list" lines to "include reftest-stylo.list"
+#   find . -name ${1}-stylo.list | xargs sed -i "s/${1}.list/${1}-stylo.list/g"
+#
+#   # TODO: ref-pref(x) test-pref(y) ... => ref-pref(y) test-pref(y) ...
+#   # Use this to check if any file becomes empty due to errors
+#   # find -name ${1}-stylo.list | xargs wc -l |sort -n -r
+# }
+#
+# # There are reftest lists that is not named reftest.list
+# # Use this script to find them:
+# # find . -name reftest.list | xargs grep "^include" -h | grep -v "reftest.list" | sort | uniq
+#
+# REFTEST_LISTS="
+# reftest
+# default-preferences-tests
+# reftest_border_abspos
+# reftest_border_parent
+# reftest_margin_abspos
+# reftest_margin_parent
+# reftest_padding_abspos
+# reftest_padding_parent
+# reftest_plain
+# scripttests
+# urlprefixtests
+# "
+#
+# for i in ${REFTEST_LISTS}; do
+#   echo "Processing ${i}"
+#   create_stylo_reftestlist ${i}
+# done
+#
+# for i in ${REFTEST_LISTS}; do
+#   for j in ${REFTEST_LISTS}; do
+#     echo "Replacing  string ${j} in all ${i}.list"
+#     find . -name ${i}-stylo.list | xargs sed -i "s/${j}.list/${j}-stylo.list/g"
+#   done
+# done
 
 disable_crash() {
   find . -name *-stylo.list | xargs grep "${1}" -l  | xargs sed -i "/$1/s/^/skip /"
 }
 
+# CRASHES="
+# bug863728-2.html
+# 212563-2.html
+# 376484-1.html
+# 381746-1.html
+# 445004-1.html
+# 482659-1d.html
+# display-contents-style-inheritance-1-dom-mutations.html
+# block-xhtml-root-1b.xhtml
+# block-xhtml-root-2.xhtml
+# block-xhtml-root-3.xhtml
+# pseudo-element-of-native-anonymous.html
+# modify-range.html
+# dom-mutations.html
+# deferred-anim-1.xhtml
+# deferred-tree-1.xhtml
+# event-target-non-svg-1.xhtml
+# cross-container-1.xhtml
+# cross-container-3.xhtml
+# calc-in-media-queries-001.html
+# calc-in-media-queries-002.html
+# ruby-inlinize-blocks-002.html
+# 1267937-1.html
+# stacking-context-opacity-changing-keyframe.html
+# stacking-context-opacity-changing-target.html
+# stacking-context-transform-changing-keyframe.html
+# stacking-context-transform-changing-target.html
+# block-xhtml-root-1a.xhtml
+# stress-3.html
+# 474472-1.html
+# 1161752.html
+# text-shadow-on-selection-1.html
+# aja-linear-3a.html
+# 212563-1.html
+# bug608373-1.html
+# 413292-1.html
+# stacking-context-transform-changing-display-property.html
+# splitText-normalize.html
+# list-1.html
+# text-shadow-on-selection-2.html
+# input-transition-1.html
+# meter-vlr-orient-vertical.html
+# bug599320-1.html
+# background-clip-text-1a.html
+# 480880-1c.html
+# 482659-1c.html
+# 1246046-1.html
+# 388980-1.html
+# bug945215-2.html
+# aja-linear-1d.html
 CRASHES="
-bug863728-2.html
-212563-2.html
-376484-1.html
-381746-1.html
-445004-1.html
-482659-1d.html
-display-contents-style-inheritance-1-dom-mutations.html
-block-xhtml-root-1b.xhtml
-block-xhtml-root-2.xhtml
-block-xhtml-root-3.xhtml
-pseudo-element-of-native-anonymous.html
-modify-range.html
-dom-mutations.html
-deferred-anim-1.xhtml
-deferred-tree-1.xhtml
-event-target-non-svg-1.xhtml
-cross-container-1.xhtml
-cross-container-3.xhtml
-calc-in-media-queries-001.html
-calc-in-media-queries-002.html
-ruby-inlinize-blocks-002.html
-1267937-1.html
-stacking-context-opacity-changing-keyframe.html
-stacking-context-opacity-changing-target.html
-stacking-context-transform-changing-keyframe.html
-stacking-context-transform-changing-target.html
-block-xhtml-root-1a.xhtml
-stress-3.html
-474472-1.html
-1161752.html
-text-shadow-on-selection-1.html
-aja-linear-3a.html
-212563-1.html
-bug608373-1.html
-413292-1.html
-stacking-context-transform-changing-display-property.html
-splitText-normalize.html
-list-1.html
-text-shadow-on-selection-2.html
-input-transition-1.html
-meter-vlr-orient-vertical.html
-bug599320-1.html
-background-clip-text-1a.html
-480880-1c.html
-482659-1c.html
-1246046-1.html
-388980-1.html
-bug945215-2.html
-aja-linear-1d.html
+link-surrounding.svg
+display-contents-shadow-dom-1.html
+343538-1.html
+plugin-sanity.html
+508908-1.xul
+dynamic-insertion-point-distribution-2.html
+shadow-dom.html
+focus-handling.html
+plugin-alpha-zindex.html
+541406-1.html
+dynamic-insertion-point-distribution-2.html
+moz-css21-block-page-break-inside-avoid-7.html
+persian-2.html
+simple-underline-scaled.svg
 "
 
 for i in $CRASHES;
--- mark_expected_fails.sh
+++ mark_expected_fails.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
 
+mark_as_skip() {
+  echo "crash ${1}"
+  grep -R "${1}" . --include *-stylo.list -l | xargs -L1 sed -i "/$1/s/^/# /"
+}
+
 mark_as_fail() {
   grep -R "${1}" . --include *-stylo.list -l | xargs -L1 sed -i "/$1/s/^/fails /"
 }
 
+unmark_as_fail() {
+  grep -R "${1}" . --include *-stylo.list -l | xargs -L1 sed -i "/$1/s/^fails //"
+}
+
 # Too many arguments breaks xargs, so we use for loop
+for i in $(grep 'CRASH' "${1}" | awk -F"|" '{print $2}' | xargs -L1 -I{} basename {})
+do
+  mark_as_skip "${i}"
+done
+
 for i in $(grep 'UNEXPECTED-FAIL' "${1}" | grep -Po '(?<=\| )(.*)(?===)' | xargs -L1 -I{} basename {})
 do
-  echo "${i}"
+  echo "fails ${i}"
   mark_as_fail "${i}"
 done
+
+# for i in $(grep 'UNEXPECTED-PASS' "${1}" | grep -Po '(?<=\| )(.*)(?===)' | xargs -L1 -I{} basename {})
+# do
+#   echo "passes ${i}"
+#   unmark_as_fail "${i}"
+# done
 
 while grep -R "fails fails " --include *-stylo.list -q
 do
   echo "Clean up multiple fails"
   find . -name *-stylo.list | xargs sed -i 's/fails fails /fails /g'
 done
+while grep -R "skip skip " --include *-stylo.list -q
+do
+  echo "Clean up multiple skips"
+  find . -name *-stylo.list | xargs sed -i 's/skip skip /skip /g'
+done
 
 find . -name *-stylo.list | xargs sed -i 's/^fails #/#/g'
+find . -name *-stylo.list | xargs sed -i 's/^skip #/#/g'
 find . -name *-stylo.list | xargs sed -i 's/^fails load/skip load/g'
+find . -name *-stylo.list | xargs sed -i 's/^skip fails /skip /g'
 # Manually change "fails (.*) load" to skip
